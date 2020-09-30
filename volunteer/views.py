a70152e9c19f969013b4a034d7ff963b4479d98b
Jamie Ryan <jlr9au@virginia.edu>
2020-09-30 16:57:12 -0400

    add schedule branch and add page for schedule

--- volunteer/views.py
+++ volunteer/views.py
@@ -7,3 +7,6 @@ def index(request):
     # template = loader.get_template('volunteer/index.html')
     # return HttpResponse(template.render())
     return render(request, 'volunteer/index.html', None)
+
+def myschedule(request):
+    return render(request, 'volunteer/myschedule.html', None)
