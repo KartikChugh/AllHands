f258b1fed4715701bd3d2cec49ec79986f58472c
KartikChugh <kartikchugh.kc@gmail.com>
2020-10-03 16:15:52 -0400

    Fix schedule URL issue
    Previously:
    - http://127.0.0.1:8000/schedule worked
    - http://127.0.0.1:8000/schedule/ did not work
    
    Now:
    Both work

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('schedule', views.myschedule, name='myschedule')
+    path('schedule/', views.myschedule, name='myschedule')
 ]
