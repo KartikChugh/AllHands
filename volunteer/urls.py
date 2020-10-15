e6b9724018ff8bb698a28b96d7147ca12850e2e0
Christopher Hamilton <christopherarthamilton@gmail.com>
2020-10-14 20:59:45 -0400

    Add Login Page
    This adds a login page and updates the path of the urls to match.

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('schedule/', views.myschedule, name='myschedule'),
-    path('post/', views.createpost, name='createpost')
+    path('', views.login, name='login'),
+    path('volunteer/', views.index, name='index'),
+    path('volunteer/schedule/', views.myschedule, name='myschedule'),
+    path('volunteer/post/', views.createpost, name='createpost')
 ]
--- volunteer/views.py
+++ volunteer/views.py
@@ -3,13 +3,20 @@ from django.http import HttpResponse
 from django.template import loader
 
 
+def login(request):
+    return render(request, 'volunteer/login.html', None)
+
+
 def index(request):
     # template = loader.get_template('volunteer/index.html')
     # return HttpResponse(template.render())
     return render(request, 'volunteer/index.html', None)
 
+
 def myschedule(request):
     return render(request, 'volunteer/myschedule.html', None)
 
+
 def createpost(request):
     return render(request, 'volunteer/createpost.html', None)
+
