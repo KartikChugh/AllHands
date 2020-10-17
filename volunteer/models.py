a530be8f6eb4228e04f737ff891f6a7f98bee117
KartikChugh <kartikchugh.kc@gmail.com>
2020-10-17 13:10:44 -0400

    Fix Volunteer Event not extending Model

--- volunteer/models.py
+++ volunteer/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class VolunteerEvent:
+class VolunteerEvent(models.Model):
     event_title = models.CharField(max_length=25)
     event_datetime = models.DateTimeField()
