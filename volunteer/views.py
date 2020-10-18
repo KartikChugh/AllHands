0560564650de17d0700fd7ae8892edb86a7d8f5a
KartikChugh <kartikchugh.kc@gmail.com>
2020-10-18 11:34:17 -0400

    Add success_url for creating event

--- volunteer/views.py
+++ volunteer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import VolunteerEvent
 
 
@@ -17,5 +18,4 @@ class CreateVolunteerEventView(generic.CreateView):
     model = VolunteerEvent
     template_name = 'volunteer/createpost.html'
     fields = ['event_title', 'event_datetime']
-# def createpost(request):
-#     return render(request, 'volunteer/createpost.html', None)
+    success_url = reverse_lazy('volunteer:createpost') # use lazy to avoid circular import error
