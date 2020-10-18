cd1ac898d2107940dba2f46a40106d149896b50d
KartikChugh <kartikchugh.kc@gmail.com>
2020-10-18 00:43:19 -0400

    Create view for creating event

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('schedule/', views.myschedule, name='myschedule'),
-    path('post/', views.createpost, name='createpost')
+    path('post/', views.CreateVolunteerEventView.as_view(), name='createpost')
 ]
--- volunteer/views.py
+++ volunteer/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views import generic
+from .models import VolunteerEvent
 
 
 def index(request):
@@ -11,5 +13,9 @@ def index(request):
 def myschedule(request):
     return render(request, 'volunteer/myschedule.html', None)
 
-def createpost(request):
-    return render(request, 'volunteer/createpost.html', None)
+class CreateVolunteerEventView(generic.CreateView):
+    model = VolunteerEvent
+    template_name = 'volunteer/createpost.html'
+    fields = ['event_title', 'event_datetime']
+# def createpost(request):
+#     return render(request, 'volunteer/createpost.html', None)
