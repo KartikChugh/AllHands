62112f18de9e226591539a5ddeeabb0fb4c82979
Christopher Hamilton <christopherarthamilton@gmail.com>
2020-11-03 14:41:20 -0500

    Reformat Tests into file structure

---
+++ tests/test_sanity.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
+
+    def setUp(self):
+        # print("setUp: Run once for every test method to setup clean data.")
+        pass
+
+    def test_false_is_false(self):
+        # print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        # print("Method: test_false_is_true.")
+        self.assertTrue(True)
+
+    def test_one_plus_one_equals_two(self):
+        # print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
+
+
+class DummyTestCase(TestCase):
+    def setUp(self):
+        x = 1
+
+    def test_dummy_test_case(self):
+        self.assertEqual(1, 1)
