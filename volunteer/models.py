99717d289db253c5e325d251e832ff534f053cd8
Rachel Lew <rel7dqj@virginia.edu>
2020-11-08 21:18:37 -0500

    adding event location and tags .. an attempt

--- volunteer/models.py
+++ volunteer/models.py
@@ -9,6 +9,9 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.urls import reverse
 from django.utils import timezone
+
+from taggit.managers import  TaggableManager
+
 class VolunteerEvent(models.Model):
 
     # tags_choices =[
@@ -19,6 +22,7 @@ class VolunteerEvent(models.Model):
     event_title = models.CharField(max_length=45)
     event_datetime = models.DateTimeField(default=datetime.now)
     event_description = models.CharField(max_length=250)
+    event_location = models.CharField(max_length=100, default="Rotunda Steps")
     # event_tags = models.CharField(max_length=15, choices=tags_choices, null=True, blank=True)
     cover = models.ImageField(upload_to='images/', blank=False, null=False, default="stock/thomas.jpg")
     event_author = models.ForeignKey(
@@ -27,6 +31,9 @@ class VolunteerEvent(models.Model):
         default=1
     )
     attending=models.ManyToManyField(User, related_name='events_attending')
+    
+    TAGGIT_CASE_INSENSITIVE  = True
+    tags  = TaggableManager("Tags", "home repair, cooking, technology, yardwork, children")
     #slug=models.SlugField(null=True)
     def __str__(self):
         return self.event_title
