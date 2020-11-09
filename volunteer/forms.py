92b2f7b00bd8732e159c627c81ddf5d9da9b0f28
Rachel Lew <rel7dqj@virginia.edu>
2020-11-08 21:27:21 -0500

    adding new fields to form

--- volunteer/forms.py
+++ volunteer/forms.py
@@ -5,13 +5,15 @@ from .models import VolunteerEvent
 class PostForm(forms.ModelForm):
     class Meta:
         model = VolunteerEvent
-        fields = ['event_title', 'event_description', 'cover', 'event_datetime']
+        fields = ['event_title', 'event_description', 'cover', 'event_datetime', 'event_location', 'tags']
 
         labels = {
             'event_title': ('Title'),
             'event_description': ('Description'),
             'cover':('Cover Photo'),
-            'event_datetime':('Date and Time')
+            'event_datetime':('Date and Time'),
+            'event_location':('Location'),
+            'tags':('Useful Tags')
         }
 
         help_texts = {
