59e105c54ca01a591bd8759e25651d7f77b6396e
KartikChugh <kartikchugh.kc@gmail.com>
2020-11-10 21:01:57 -0500

    Add unregister url

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('volunteer/schedule/', views.myschedule, name='myschedule'),
     path('volunteer/post/', views.CreateVolunteerEventView.as_view(), name='createpost'),
     path('volunteer/signup/<pk>', views.signup, name='signup'),
+    path('volunteer/unregister/<pk>', views.unregister, name='unregister'),
     path('volunteer/events/<pk>', views.DetailView.as_view(), name='detail'),
     path('volunteer/myevents/', views.myevents, name='myevents'),
     path('volunteer/myevents/<pk>', views.EventDetailView.as_view(), name='detailmyevent'),
